438a540d8cc1a50812d5ba504e7999ccd9f16d4b
Marcin.Wegrzyn <wegrzyn.marcin@icloud.com>
2017-11-01 14:09:06 +0100

    zadanie do sprawdzenia

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,22 @@
 // scripts.js
+var a = 1,
+    b = 1,
+    value;
+value = (a * a) - (2 * a * b) + (b * b);
 
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+console.log(value);
+
+if (value >= 0) {
+  console.log('wartość dodatnia')
+}
+else {
+  console.log('wartość ujemna')
+};
+
+switch (value) {
+  case 0:
+    console.log('jest 0')
+    break
+  default:
+    console.log('liczba inna niż 0')
+}
