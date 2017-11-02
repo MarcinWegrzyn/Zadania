d69a37c8f174ce12477876e7256bee00ca21ca32
Marcin.Wegrzyn <wegrzyn.marcin@icloud.com>
2017-11-02 13:57:03 +0100

    Zadanie 9.1 do sprawdzenia

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 // scripts.js
 
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+var a,
+    h; 
+function getTriangleArea(a, h) { 
+  if ((a > 0) && (h >0)) {return a*h/2}
+  else {console.log("Nieprawidłowe dane")}
+};
+console.log(getTriangleArea(10, 6));
+
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(20, 30);
+var triangle3Area = getTriangleArea(30, 45);
+
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
