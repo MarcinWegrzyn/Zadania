7819f7c0879843b02afadbd5d2d39bbde84d9296
Marcin.Wegrzyn <wegrzyn.marcin@icloud.com>
2017-11-03 11:37:59 +0100

    zadanie 9.2 do sprawdzenia

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 // scripts.js
 
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var allNames = femaleNames.concat(maleNames);
+console.log(allNames);
+
+var newName = 'Marian';
+var newList = allNames.indexOf(newName);
+
+if (newList === -1) {allNames.push(newName)}
+else {console.log('Imie już istnieje')};
+
+console.log(allNames);
