3a719a26f7cd92bfbfc7ceef05ccc1ee9fced80b
Marcin.Wegrzyn <wegrzyn.marcin@icloud.com>
2017-11-03 23:11:34 +0100

    Zadanie do sprawdzenia

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 // scripts.js
 
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
+console.log(text);
+
+var dinosaur = 'triceratops';
+var newDinosaur = dinosaur.toUpperCase ();
+//console.log(newDinosaur);
+var newText = text.replace('Velociraptor' , newDinosaur);
+//console.log(newText.length/2);
+var shortText = newText.slice(0, (newText.length/2));
+console.log(shortText);
