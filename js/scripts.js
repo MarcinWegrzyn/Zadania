74ba705763193738db7767159ae9b0ed59c6713c
Marcin.Wegrzyn <wegrzyn.marcin@icloud.com>
2017-11-03 16:00:04 +0100

    poprawiona składnia

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,16 @@
 var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
 var allNames = femaleNames.concat(maleNames);
+
 console.log(allNames);
 
 var newName = 'Marian';
 var newList = allNames.indexOf(newName);
 
-if (newList === -1) {allNames.push(newName)}
-else {console.log('Imie już istnieje')};
+if (newList === -1) {
+	allNames.push(newName);
+} else {
+	console.log('Imię już istnieje');
+};
 
 console.log(allNames);
