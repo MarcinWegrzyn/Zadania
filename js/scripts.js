f9604869c6317a97403cfa2b2794b096d60c3f00
Marcin.Wegrzyn <wegrzyn.marcin@icloud.com>
2017-11-06 20:55:59 +0100

    poprawiona choinka

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,13 @@ function drawTree(x) {
     for ( var i = 0 ; i < x ; i ++) {
       var space="";
       var star ="";
-        for ( var j = 0; j < i; j ++) {  // Tutaj warunek środkowy już nie będzie taki prosty :)  trzeba będzie troszkę inaczej do tego podejść.
+        for ( var j = 0; j < (i*2 +1); j ++) {
            star = star += "*";
-            for (var z = i ; z < x ; z ++) {   // Ta pętla powinna być zaraz pod pętlą z licznikiem  j. Nie w niej.
-             space = space += " ";
-            }
-         }
+        }
+          for (var z = i ; z < x ; z ++) {
+           space = space += " ";
+        }    
       console.log(space + star);
     }
 }
-
-drawTree(10);
+drawTree(5);
