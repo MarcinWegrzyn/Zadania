c566dce5db6dba77c8974b262f13b01b23413091
Michał Gołębiowski <michal.golebiowski@unity.pl>
2017-11-06 19:15:54 +0100

    CR

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 // scripts.js
 
-function drawTree(x) { 
+function drawTree(x) {
     for ( var i = 0 ; i < x ; i ++) {
       var space="";
       var star ="";
-        for ( var j = 0; j < i; j ++) {
+        for ( var j = 0; j < i; j ++) {  // Tutaj warunek środkowy już nie będzie taki prosty :)  trzeba będzie troszkę inaczej do tego podejść.
            star = star += "*";
-            for (var z = i ; z < x ; z ++) {
+            for (var z = i ; z < x ; z ++) {   // Ta pętla powinna być zaraz pod pętlą z licznikiem  j. Nie w niej.
              space = space += " ";
             }
          }
