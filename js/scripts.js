e3d95395d3ee03a836ca967050b491b26ba404f9
Marcin.Wegrzyn <wegrzyn.marcin@icloud.com>
2017-11-14 22:28:39 +0100

    v.01

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
-// scripts.js
+$(function(){
+  
+  var carouselList = $("#carousel ul");
+   
+      setInterval(changeSlide, 2500);
+      carouselList.animate({'marginLeft':-640}, 500, moveFirstSlide);
+        
 
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+  function changeSlide(){
+    var firstItem = carouselList.find("li:first"),
+        lastItem = carouselList.find("li:last");
+        lastItem.after(firstItem);
+        carouselList.css({marginLeft:0});
+  }
+
+});
