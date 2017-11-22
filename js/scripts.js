d9f34fd98bd0ca6104f05b762f4c3e443c027ddf
Marcin.Wegrzyn <wegrzyn.marcin@icloud.com>
2017-11-22 22:21:46 +0100

    ver.06

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,38 @@
 $(function(){
   
   var carouselList = $("#carousel ul");
-  var slideInterval = setInterval(changeSlide, 3000);
+  var slideInterval = setInterval(changeSlide, 1000);
 
   $("#carousel").mouseover(function() {
   clearInterval(slideInterval);
   });
 
+  $("#carousel").mouseout(function() {
+  slideInterval = setInterval(changeSlide, 1000);
+  });
+
   function changeSlide(){
-    carouselList.animate({'marginLeft':-640}, 500, moveFirstSlide);
+    carouselList.animate({'marginLeft':-1280}, 500, moveFirstSlide);
 
     function moveFirstSlide(){
       var  firstItem = carouselList.find("li:first"),
            lastItem = carouselList.find("li:last");
       lastItem.after(firstItem);
-      carouselList.css({marginLeft:0});
+      carouselList.css({marginLeft:-640});
     }
   }
 
-  function slideBack(){
-    carouselList.animate({'marginLeft':+640}, 500, moveFirstSlideBack);
+   function slideBack(){
+
+    carouselList.animate({'marginLeft':0}, 500, moveFirstSlideBack);
 
     function moveFirstSlideBack(){
      var  firstItem = carouselList.find("li:first"),
           lastItem = carouselList.find("li:last");
       firstItem.before(lastItem);
-      carouselList.css({marginLeft:0});
+      carouselList.css({marginLeft:-640});
     }
-  }
+  } 
 
   $("#right").click(function(){
     changeSlide();
