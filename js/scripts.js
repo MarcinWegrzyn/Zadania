6ee91a4b3cb1ddb1bb195acaa2009dd31f2d4991
Marcin.Wegrzyn <wegrzyn.marcin@icloud.com>
2017-11-23 22:56:15 +0100

    v.01

--- js/scripts.js
+++ js/scripts.js
@@ -13,8 +13,22 @@ Phone.prototype.printInfo = function() {
 
 var GalaxyS7 = new Phone ('Samsung', 2000, 'blue', 2015),
     IphoneX = new Phone ('Apple', 1000000, 'black', 2017),
-    N3210 = new Phone ('Nokia', 200, 'red', 2000); // Czy var może być liczbą? Jeżeli tak to czy daje ją jako string '3210' ?
+    N3210 = new Phone ('Nokia', 200, 'red', 2000); // Czy var może być liczbą? Jeżeli tak to czy daję ją jako string '3210' ?
 
 N3210.printInfo();
 IphoneX.printInfo();
 GalaxyS7.printInfo();
+
+Phone.prototype.printPrice = function() {
+    console.log( 'Worth of all 3 phones is ' + (IphoneX.price + N3210.price + GalaxyS7.price) +' PLN');
+  }
+
+var Sum = new Phone();
+Sum.printPrice();
+
+
+
+
+
+
+
